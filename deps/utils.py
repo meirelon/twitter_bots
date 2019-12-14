1282e309873c67b89e27b4b36e3416edacfe2526
meirelon <nestelm@gmail.com>
2019-12-14 01:43:40 -0500

    initial commit

---
+++ deps/utils.py
@@ -0,0 +1,11 @@
+from gcloud import storage
+from tempfile import NamedTemporaryFile
+
+def load_pipeline(project_id, bucket, destination_path, filename):
+    client = storage.Client(project=project_id)
+
+    with NamedTemporaryFile(mode='rb') as tempfile:
+        gcs_path = os.path.join(destination_path, '{filename}.pkl'.format(filename=filename))
+        client.bucket(bucket).blob(gcs_path).download_to_filename(tempfile.name)
+        tempfile.seek(0)
+        return pickle.load(tempfile)
---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import pickle
+from datetime import datetime
+from gcloud import storage
+from tempfile import NamedTemporaryFile
+
+from deps.utils import load_pipeline
+
+
+
+def bernie_tweet(request):
+    project = os.environ["PROJECT_ID"]
+    bucket = os.environ["BUCKET"]
+    destination_path = os.environ["DESTINATION_PATH"]
+    filename = os.environ["FILENAME"]
+
+    bot = load_pipeline(project_id=project,
+                        bucket=bucket,
+                        destination_path=destination_path=,
+                        filename=filename)
+
+    return bot.make_short_sentence(140)
