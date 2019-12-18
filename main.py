9391a7f9dfa9bd919c17189422c5045d3992aa2d
meirelon <nestelm@gmail.com>
2019-12-17 20:36:45 -0500

    updated with tank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import random
 import requests
 from datetime import datetime
 from gcloud import storage
@@ -29,8 +30,7 @@ def bernie_tweet(request):
 
     return tweet
 
-
-def tweet_send(request):
+def bernie_tweet_send(request):
     #gcp
     project = os.environ["PROJECT_ID"]
     #twitter
@@ -49,5 +49,47 @@ def tweet_send(request):
 
     r = requests.get("https://us-central1-{project}.cloudfunctions.net/{tweet_type}".format(project=project, tweet_type=tweet_type))
     api.update_status(r.text)
+    # update status
+    return "success"
+
+
+def tank_tweet_send(request):
+    #gcp
+    project = os.environ["PROJECT_ID"]
+    bucket = os.environ["BUCKET"]
+    destination_path = os.environ["DESTINATION_PATH"]
+    filename = os.environ["FILENAME"]
+    #twitter
+    consumer_key = os.environ["CONSUMER_KEY"]
+    consumer_secret = os.environ["CONSUMER_SECRET"]
+    access_token = os.environ["ACCESS_TOKEN"]
+    access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
+    # tweet_type = os.environ["TWEET_TYPE"]
+
+    filenames = filename.split(" ")
+    value = random.randint(0,1)
+    if len(filenames)>1:
+        filename = filenames[value]
+    bot = load_pipeline(project_id=project,
+                        bucket=bucket,
+                        destination_path=destination_path,
+                        filename=filename)
+    try:
+        tweet = bot.make_short_sentence(140)
+    except:
+        tweet = ""
+
+    twitter = twitterApi(consumer_key=consumer_key,
+                     consumer_secret=consumer_secret,
+                     access_token=access_token,
+                     access_token_secret=access_token_secret)
+
+    api = twitter.twitter_auth()
+
+    # r = requests.get("https://us-central1-{project}.cloudfunctions.net/{tweet_type}".format(project=project, tweet_type=tweet_type))
+    # api.update_status(r.text)
+    api.update_status(tweet)
+
+
     # update status
     return "success"
